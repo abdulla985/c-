ea463a858492f7f8f9034c3c6b17c2cca3a9ee71
abdulla985 <122654462+abdulla985@users.noreply.github.com>
2023-10-31 00:06:48 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include<iostream>
 using namespace std;
 int main(){
-
+float a,b,
+char op,
+cout<<"enter first number "<<endl;
+cin>>a;
+cout<<"enter operator "<<endl;
+cin>>op;
+cout<<"enter second number "<<endl;
+switch (op){
+  case '+':
+  cout<<a+b;
+  break;
+  case '-':
+  cout<<a-b;
+  break;
+  case '*':
+  cout<<a*b;
+  break;
+  case '/':
+  cout<<a/b;
+  break:
+}
 return 0;
 }
